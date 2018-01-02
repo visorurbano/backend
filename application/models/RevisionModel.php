f26ee9a79cabf0cac4610353c0fa421ea98f26d9
luiskgarcia <egarcia@visorurbano.com>
2018-01-02 17:22:44 -0600

    actual pdf, review

--- application/models/RevisionModel.php
+++ application/models/RevisionModel.php
@@ -1,8 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class RevisionModel extends CI_Model {
-    public function getLicencias($idUser, $todas, $revisadas, $prioritarios){
-        $query=$this->db->query('select * from tbl_licencias_giro where id_usuario='.$idUser.' and status="FL"');
+    public function getLicencias($idUser,$Tipo){
+        switch ($Tipo) {
+          case 'T':
+              $query=$this->db->query('select * from tbl_licencias_giro where id_usuario='.$idUser.' and status="FL"');
+            break;
+          case 'R':
+              $query=$this->db->query('select * from tbl_licencias_giro where id_usuario='.$idUser.' and status="FL" and revisada = 1');
+            break;
+          case 'P':
+              $query=$this->db->query('select * from tbl_licencias_giro where id_usuario='.$idUser.' and status="FL" and revisada = 1');
+            break;
+        }
         if($query->num_rows()>0){
             $resultado=array("status"=>true,"data"=>$query->result());
         }else{
--- assets/js/revision.js
+++ assets/js/revision.js
@@ -3,6 +3,17 @@ var userURL = 'http://licenciasprueba.guadalajara.gob.mx/gdlusuarios/auth/';
 var baseURL = "http://localhost/backend/";
 //var userURL = "http://localhost/usrGDL/auth/";*/
 function incializarTbl(val){
+  switch (val) {
+    case "T":
+        val="Todas";
+      break;
+    case "R":
+        val="Revisadas";
+      break;
+    case "P":
+        val="Prioritarios";
+      break;
+   }
     $('#tbl'+val).DataTable({
         "ordering": false,
         "info":     false,
@@ -13,46 +24,69 @@ function incializarTbl(val){
             "sZeroRecords": "No se encontrarón coincidencias :(",
         },
         "language": {
-            "zeroRecords": "No records to display"
+            "sZeroRecords": "No records to display"
         }
     });
 }
 
-function todas(){
-    $.ajax({
-        url: baseURL + "RevisionController/licencias",
-        dataType:'json',
-        type: 'POST',
-        data:{
-            "todas":'S',
-        },
-        success:function(data){
-            console.log(data);
-        },
+function encode(pass){
+   var sha256 = new jsSHA('SHA-256', 'TEXT');
+   sha256.update(pass);
+   var hash = sha256.getHash("HEX");
+   return [hash];
+}
+
+function traer_informacion(val){
+  $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: baseURL + "RevisionController/licencias",
+      data:{
+        "tipo":val,
+      },
+      success:function(data){
+          var identificador;
+          var idTablas;
+          switch (val) {
+            case "T":
+            identificador=$('#bodyTodas');
+            idTablas="Todas";
+            break;
+            case "R":
+            identificador=$('#bodyRevisadas');
+            idTablas="Revisadas";
+            break;
+            case "P":
+            identificador=$('#bodyPrioritarios');
+            idTablas="Prioritarios";
+            break;
+          }
+          if(data.status){
+             for (var i = 0; i < data.data.length; i++) {
+               identificador.append('<tr role="row">'+
+               '<td>'+
+               data.data[i].st2_nombre_solicitante+
+               '</td>'+
+               '<td>'+
+               data.data[i].descripcion_factibilidad+
+               '</td>'+
+               '<td>'+
+               data.data[i].fecha+
+               '</td>'+
+               '<td style="text-align:center;">'+
+               '<a href="/revision"><i class="fa fa-eye" aria-hidden="true"></i></a>'+
+               '</td>'+
+               '</tr>');
+             }
+              incializarTbl(val);
+          }else{
+            identificador.append('<tr role="row">'+
+            '<td colspan="4" style="text-align:center;">'+
+              "No se encontrarón licencias :("+
+            '</td>'+
+            '</tr>');
+          }
+    }
     });
-    $('#bodyTodas').append('<tr role="row">'+
-        '<td>'+
-            'AFDSDFGASD'+
-        '</td>'+
-        '<td>'+
-            'HGDFHDSGFHJ'+
-        '</td>'+
-        '<td>'+
-            'DSKFJKHDSFDS'+
-        '</td>'+
-        '<td style="text-align:center;">'+
-            '<a href="#"><i class="fa fa-eye" aria-hidden="true"></i></a>'+
-        '</td>'+
-    '</tr>');
-    incializarTbl('todas');
 }
-
-function revisadas(){
-    $('#tblrevisadas').append();
-}
-
-function prioritarios(){
-    $('#tblprioritarios').append();
-}
-
-todas();
+traer_informacion('T');
